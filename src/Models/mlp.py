d1a1a75ca1bee263a49d2276b135588f97d90134
lachy <lachygmain@gmail.com>
2025-09-18 16:28:42 +0800

    simplified hparams

--- src/Models/mlp.py
+++ src/Models/mlp.py
@@ -4,13 +4,15 @@ from torchvision import transforms
 from Utils.model_utils import str_to_activation, str_to_optimiser
 
 class MLP(nn.Module):
-    def __init__(self, output_size, widths=[512, 256], dropout=0.3, lr=4e-4, weight_decay=3e-2, activation_fn=nn.ReLU, optimiser=torch.optim.AdamW):
+    def __init__(self, output_size, width=512, depth=3, funnel_factor=1, dropout=0.3, lr=4e-4, weight_decay=3e-2, activation_fn=nn.ReLU, optimiser=torch.optim.AdamW, min_width=64):
         super().__init__()
 
+        w = width   # set intial
         # create network
         layers = [nn.Flatten()]
-        for w in widths:
-            layers += [nn.LazyLinear(w), activation_fn, nn.Dropout(dropout)]
+        for i in range(depth):
+            layers += [nn.LazyLinear(w), nn.ReLU(), nn.Dropout(dropout)]
+            w = max(w//funnel_factor, min_width)  # update width for next iter 
         layers += [nn.LazyLinear(output_size)]
 
         self.net = nn.Sequential(*layers)
@@ -49,25 +51,14 @@ class MLP(nn.Module):
         batch_size = trial.suggest_categorical("batch_size", [16, 32, 64, 128, 256])
 
         depth = trial.suggest_int("depth", 1, 6)
-        widths = [trial.suggest_categorical(f"width_{i}", [64, 128, 256, 512, 1024, 2048]) for i in range (depth)]
+        width = trial.suggest_categorical("width", [64, 128, 256, 512, 1024, 2048])
+        funnel_factor = trial.suggest_categorical("funnel_factor", [1, 2, 3])
 
         dropout = trial.suggest_float("dropout", 0.0, 0.5)
-        lr = trial.suggest_float("lr", 1e-5, 1e-2, log=True)
-        weight_decay  = trial.suggest_float("weight_decay", 1e-4, 1e-2, log=True)
+        lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
+        weight_decay  = trial.suggest_float("weight_decay", 1e-8, 1e-2, log=True)
 
         activation_fn = str_to_activation(trial.suggest_categorical('activation_fn', ['relu', 'sigmoid', 'tanh']))
         optimiser = str_to_optimiser(trial.suggest_categorical("optimiser", ["Adam", "AdamW", "SGD"]))
     
-        return cls(output_size, widths, dropout, lr, weight_decay, activation_fn, optimiser), batch_size
-
-    @classmethod
-    def extract(cls, hp):
-        hp2 = {k: v for k, v in hp.items() if k != "depth" and not k.startswith("width_")}  # rebuild hp without depth and w
-
-        widths = []
-        for k in hp:
-            if k.startswith("width_"):
-                widths.append(hp[k])
-
-        hp2["widths"] = widths
-        return hp2
+        return cls(output_size, width, depth, funnel_factor, dropout, lr, weight_decay, activation_fn, optimiser), batch_size
